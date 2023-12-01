5004203905c41d66d9f410d44040445f65c9c3ce
Austin3939 <105609013+Austin3939@users.noreply.github.com>
2023-12-01 11:53:58 -0700

    Update Global.asax.cs

--- Global.asax.cs
+++ Global.asax.cs
@@ -10,8 +10,14 @@ namespace Inscript_v5
 {
     public class MvcApplication : System.Web.HttpApplication
     {
-        protected void Application_Start()
+        protected void Application_Start(object sender, EventArgs e)
         {
+            string connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__Inscript_v5Context");
+            if (!string.IsNullOrEmpty(connectionString))
+            {
+            var connectionStringsSection = ConfigurationManager.ConnectionStrings["Inscript_v5Context"];
+            connectionStringsSection.ConnectionString = connectionString;
+            }
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
